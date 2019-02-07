1823f90a75bda010cd57e3034c724d35cef71f81
Hearty Oh <heartyoh@hatiolab.com>
2019-02-08 04:55:33 +0900

    remove export views

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 export * from './auth-router'
 export * from './client-auth'
-
-export * from './view/signin'
-export * from './view/signup'
-export * from './view/signout'
-export * from './view/profile'
